bb399b78f76ecb292f71cfe4c0d58538b9e5be24
Gustavo Souza Banegas <gustavo@cryptme.in>
2026-04-07 13:19:07 +0200

    added some general guides for curve, point, isogeny and kernel.

--- ec/src/curve.rs
+++
@@ -1,8 +0,0 @@
-//! Elliptic curve definition (short Weierstrass form).
-
-/// Short Weierstrass curve  y² = x³ + ax + b  over Fp.
-pub struct WeierstrassCurve {
-    // TODO: replace with fp::FieldElement once defined
-    pub a: u64,
-    pub b: u64,
-}
