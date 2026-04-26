13daf5d88c7d46ccd283e1b5db94010346fd060d
Sam Frengley <sam.frengley@gmail.com>
2026-04-26 11:41:06 +0200

    Fixed merge

--- ec/src/point_twisted_hessian.rs
+++ ec/src/point_twisted_hessian.rs
@@ -15,7 +15,7 @@ use subtle::{Choice, ConditionallySelectable, ConstantTimeEq};
 
 use crate::curve_twisted_hessian::TwistedHessianCurve;
 use crate::point_ops::{PointAdd, PointOps};
-use fp::field_ops::{FieldOps};
+use fp::field_ops::FieldOps;
 use fp::{ref_field_impl, ref_field_trait_impl};
 
 /// A projective point `(X:Y:Z)` on a twisted Hessian curve.
